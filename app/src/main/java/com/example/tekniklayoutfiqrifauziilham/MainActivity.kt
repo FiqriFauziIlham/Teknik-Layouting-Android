c819941b351668a2c28d348061c98a348060a6ea
FiqriFauziIlham <fiqrifauziilham1@gmail.com>
2025-02-07 13:37:37 +0700

    Notes crud

--- app/src/main/java/com/example/tekniklayoutfiqrifauziilham/MainActivity.kt
+++ app/src/main/java/com/example/tekniklayoutfiqrifauziilham/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tekniklayoutfiqrifauziilham
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,15 +23,28 @@ class MainActivity : AppCompatActivity() {
 
         val btnClose = findViewById<Button>(R.id.btn_keluar)
         val btnOpenCalculator = findViewById<Button>(R.id.btn_open_calculator)
+        val btnOpenNote = findViewById<Button>(R.id.btn_open_note)
 
         btnClose.setOnClickListener {
             finish()
         }
 
-        // Menambahkan Intent untuk membuka CalculatorActivity
         btnOpenCalculator.setOnClickListener {
-            val intent = Intent(this, CalculatorActivity::class.java)
-            startActivity(intent)
+            try {
+                val intent = Intent(this, CalculatorActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Error opening CalculatorActivity", e)
+            }
+        }
+
+        btnOpenNote.setOnClickListener {
+            try {
+                val intent = Intent(this, NoteActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Error opening NoteActivity", e)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/tekniklayoutfiqrifauziilham/Note.kt
@@ -0,0 +1,3 @@
+package com.example.tekniklayoutfiqrifauziilham
+
+data class Note(val id: Int, val title: String, val content: String)
